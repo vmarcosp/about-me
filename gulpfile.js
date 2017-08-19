0bd9f4ebb90c1079ddc025db6d89e32a98d8f0ea
vmarcosp <marcosvcontato.mv@gmail.com>
2017-08-19 03:04:08 -0300

    Added normalize and flexboxgrid to project. Configured babelify and gulp sass for development mode

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+const gulp = require('gulp'),
+    browserSync = require('browser-sync').create(),
+    sass = require('gulp-sass'),
+    browserify = require('browserify'),
+    source = require('vinyl-source-stream');
+
+gulp.task('serve', ['babelify', 'sass'], () => {
+
+    browserSync.init({
+        server: './'
+    });
+
+    gulp.watch('./scss/**/*.scss', ['sass']);
+    gulp.watch('./js/**/*.js', ['babelify']);
+    gulp.watch('./*.html').on('change', browserSync.reload);
+});
+
+gulp.task('babelify', () => {
+    return browserify('./js/src/app.js')
+        .transform('babelify', { presets: ['es2015'] })
+        .bundle()
+        .pipe(source('app.js'))
+        .pipe(gulp.dest('./js/dist/'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('sass', () => {
+    return gulp.src('./scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./css'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
