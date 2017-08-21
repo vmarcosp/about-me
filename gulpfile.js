e9b35568a43728a643850c8695b001bfa72c39b1
vmarcosp <marcosvcontato.mv@gmail.com>
2017-08-20 22:30:30 -0300

    Created tasks for build

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,12 @@ const gulp = require('gulp'),
     browserSync = require('browser-sync').create(),
     sass = require('gulp-sass'),
     browserify = require('browserify'),
-    source = require('vinyl-source-stream');
+    source = require('vinyl-source-stream'),
+    cleanCss = require('gulp-clean-css'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    htmlReplace = require('gulp-html-replace');
+
 
 gulp.task('serve', ['babelify', 'sass'], () => {
 
@@ -31,4 +36,51 @@ gulp.task('sass', () => {
         .pipe(browserSync.stream());
 });
 
+/**
+ * Task for build CSS
+ */
+
+gulp.task('css', () => {
+    return gulp.src([
+        'css/all.css',
+        'node_modules/flexboxgrid/dist/flexboxgrid.min.css',
+        'node_modules/normalize.css/normalize.css',
+    ])
+        .pipe(concat('all.min.css'))
+        .pipe(cleanCss({ compatibilty: 'ie8' }))
+        .pipe(gulp.dest('dist/css/'));
+});
+
+/**
+ * Taks for build JS
+ */
+
+gulp.task('js', () => {
+    gulp.src('js/dist/app.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
+});
+
+/**
+ * Tasks for imgs
+ */
+
+gulp.task('imgs', () => {
+    gulp.src('imgs/**/*')
+        .pipe(gulp.dest('dist/imgs'));
+});
+/**
+ * Tasks for html
+ */
+
+gulp.task('replacer', () => {
+    gulp.src('./index.html')
+        .pipe(htmlReplace({
+            'css': 'css/all.min.css',
+            'js': 'js/app.js'
+        }))
+        .pipe(gulp.dest('dist/'));
+});
+
 gulp.task('default', ['serve']);
+gulp.task('build', ['imgs', 'css', 'js', 'replacer']);
