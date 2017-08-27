24e96be1588f57040da1cad384361e6fcbf0200b
vmarcosp <marcosvcontato.mv@gmail.com>
2017-08-27 01:18:50 -0300

    Created form for contact o/

--- js/src/App.js
+++ js/src/App.js
@@ -1,15 +1,12 @@
 import BannerController from './controllers/BannerController';
-import ServicesController from './controllers/ServicesController';
 
 class App {
   constructor() {
     this.bannerController = new BannerController();
-    this.servicesController = new ServicesController();
   }
 
   init() {
     this.bannerController.init();
-    this.servicesController.init();
   }
 }
 
